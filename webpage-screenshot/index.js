255ae47cc54f0fa54d709d94417ecce1c7be328e
emanuelefavero <emafaux89@gmail.com>
2022-11-10 23:41:07 +0100

    add - save screenshot to pdf

--- webpage-screenshot/index.js
+++ webpage-screenshot/index.js
@@ -8,6 +8,7 @@ async function takeWebpageScreenshot() {
   await page.goto('https://www.apple.com')
 
   await page.screenshot({ path: 'webpage-screenshot.png', fullPage: true })
+  // await page.pdf({ path: 'webpage-screenshot.pdf', format: 'A4' })
 
   await browser.close()
 }
